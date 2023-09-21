d243885d487ce58414ce74e8bd9bd02a44773dee
ConfettiRaindrop <camilafigueira@outlook.com>
2023-09-21 13:44:37 -0700

    comment out swing, add fx documentation

--- UserFace.java
+++ UserFace.java
@@ -1,47 +1,58 @@
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JToolBar;
+//import javax.swing.JButton;
+//import javax.swing.JFrame;
+//import javax.swing.JPanel;
+//import javax.swing.JScrollPane;
+//import javax.swing.JTable;
+//import javax.swing.JToolBar;
 
-import java.awt.Dimension;
-import java.awt.GridLayout;
+//import java.awt.Dimension;
+//import java.awt.GridLayout;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+//https://fxdocs.github.io/docs/html5/#_timing
 
-public class UserFace extends JPanel{
+public class UserFace extends Application{
     public UserFace() {
-        super(new GridLayout(1, 0));
-
-        JTable table = new JTable(new MyTableModel());
-        table.setPreferredScrollableViewportSize(new Dimension(300, 600));
-        table.setFillsViewportHeight(true);
-
-        JScrollPane scrollPane = new JScrollPane(table);
-        add(scrollPane);
+        //super(new GridLayout(1, 0));
+        super();
+//
+        //JTable table = new JTable(new MyTableModel());
+        //table.setPreferredScrollableViewportSize(new Dimension(300, 600));
+        //table.setFillsViewportHeight(true);
+//
+        //JScrollPane scrollPane = new JScrollPane(table);
+        //add(scrollPane);
     }
 
     public static void createAndShowGUI() {
-        JFrame frame = new JFrame("(┬┬﹏┬┬)");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    
-        UserFace newContentPane = new UserFace();
-        newContentPane.setOpaque(true);
-        frame.setContentPane(newContentPane);
-    
-        frame.pack();
-        frame.setVisible(true);
+        //JFrame frame = new JFrame("(┬┬﹏┬┬)");
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //
+        //UserFace newContentPane = new UserFace();
+        //newContentPane.setOpaque(true);
+        //frame.setContentPane(newContentPane);
+    //
+        //frame.pack();
+        //frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-              createAndShowGUI();
-            }
-          });
+        //javax.swing.SwingUtilities.invokeLater(new Runnable() {
+        //    public void run() {
+        //      createAndShowGUI();
+        //    }
+        //  });
     }
 
-    //future Camila look at this: https://careerkarma.com/blog/javafx-vs-java-swing/
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
     
-}
+} 
