ddef40247d23bd90b8ad105199fb88da5ae7bd91
Sofiya Chubich <95042138+SofiyaChubich@users.noreply.github.com>
2023-09-21 19:59:19 +0000

    the m stays

--- CalculatorFunctions.java
+++ CalculatorFunctions.java
@@ -1,1 +1,24 @@
+public class CalculatorFunctions (){
 
+    public static double summ(double a, double b){
+        return a + b;
+    }
+
+    public static double subtract(double a, double b){
+        return a - b;
+    }
+
+    public static double multiply(double a, double b){
+        return a * b;
+    }
+
+    public static double divide(double a, double b){
+        return a / b;
+    }
+
+
+    public static void main(String[] args){
+        
+    }
+
+}
