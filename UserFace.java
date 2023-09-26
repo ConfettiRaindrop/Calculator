c4b1e58835b8ce050cf096101c55edfbc7c0de9d
ConfettiRaindrop <camilafigueira@outlook.com>
2023-09-26 13:14:11 -0700

    add button, start gridLayout

--- UserFace.java
+++ UserFace.java
@@ -5,25 +5,42 @@ import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 //https://fxdocs.github.io/docs/html5/#_timing
 
 public class UserFace extends Application{
-    public UserFace() {
 
-    }
+    Button button;
+    Button addition;
 
     public static void main(String[] args) {
-
+        launch(args);
     }
 
-    @Override
-    public void start(Stage arg0) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'start'");
-    }
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Calculator of Rage");
 
+        button = new Button();
+        button.setText("Rage Against the Machine");
+        addition = new Button();
+        addition.setText("+");
+
+        StackPane layout = new StackPane();
+        StackPane columnOne = new StackPane();
+        StackPane columnTwo = new StackPane();
+        layout.getChildren().add(columnOne);
+        layout.getChildren().add(columnTwo);
+        GridPane gridPane = new GridPane();
+        columnOne.getChildren().add(gridPane);
+        gridPane.getChildren().add(button);
+        gridPane.getChildren().add(addition);
+
+        Scene scene = new Scene(layout, 300, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     
 } 
