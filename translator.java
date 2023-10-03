a8ed086eeb7988485c1b85fce67dd7347b70e860
Sindhura-Satti <89949740+Sindhura-Satti@users.noreply.github.com>
2023-10-03 23:03:58 +0000

    splitted input and started parsing

--- translator.java
+++ translator.java
@@ -1,34 +1,33 @@
-public class Translator{
+import java.util.ArrayList; 
 
-    public Translator(){
+public class Translator {
+    Translator(){
+        System.out.println("Hello world"); 
         CalculatorFunction calc = new CalculatorFunction(); 
-
+        String u_input = calc.getUserInput(); 
+    }
+  public static void main(String[] args) {
+  	ArrayList<Integer> nums = new ArrayList<Integer>(); 
+  	ArrayList<Char> operators = new ArrayList<Char>(); 
+      char[] chars = u_input.toCharArray();
+      for (char n : chars){
+          if (Charecter.isDigit(n)){
+              nums.add(n);
+          }else if()
+      }
+      if (String.valueOf(character).matches("[^a-zA-Z0-9]")) {
+        //Your code
+    }
+    
+    System.out.println("Hello World");
+  }
+  
+  public static int recurSolve(String u){
+    if (u_input.length() == 1){
+        //base case - 1 digit left 
+    }else if (Charecter.isDigit(u_input.substring(0,1)){
 
     }
 
-    public static int solve (String u_input){
-        ArrayList<char> nums = new ArrayList<>(); 
-        ArrayList<char> operators = new ArrayList<>(); 
-        
-        char[] chars = u_input.toCharArray();
-        for (char n : chars){
-            if (Charecter.isDigit(n)){
-                
-            }
-        }
-        
-    }
-
-    public static int recurSolve(String u){
-        if (u_input.length() == 1){
-            //base case - 1 digit left 
-        }else if (Charecter.isDigit(u_input.substring(0,1)){
-
-        }
-
-    public static void main(String[] args) {
-        System.out.println(calc.summ(3,4));
-    }
-    }
-
+}
 }
