1e0ed6f8bc6108a4010148013df543e777440bf5
Sindhura-Satti <89949740+Sindhura-Satti@users.noreply.github.com>
2023-10-03 20:08:30 +0000

    hey im back

---
+++ translator.java
@@ -0,0 +1,34 @@
+public class Translator{
+
+    public Translator(){
+        CalculatorFunction calc = new CalculatorFunction(); 
+
+
+    }
+
+    public static int solve (String u_input){
+        ArrayList<char> nums = new ArrayList<>(); 
+        ArrayList<char> operators = new ArrayList<>(); 
+        
+        char[] chars = u_input.toCharArray();
+        for (char n : chars){
+            if (Charecter.isDigit(n)){
+                
+            }
+        }
+        
+    }
+
+    public static int recurSolve(String u){
+        if (u_input.length() == 1){
+            //base case - 1 digit left 
+        }else if (Charecter.isDigit(u_input.substring(0,1)){
+
+        }
+
+    public static void main(String[] args) {
+        System.out.println(calc.summ(3,4));
+    }
+    }
+
+}
