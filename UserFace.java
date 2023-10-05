0940dc17d1525880f2d7e7efd5a6f4cc05db805f
ConfettiRaindrop <camilafigueira@outlook.com>
2023-10-05 12:48:12 -0700

    remove unused imports

--- UserFace.java
+++ UserFace.java
@@ -4,16 +4,13 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import java.util.Random;
-import javafx.stage.Stage;
 
 //https://fxdocs.github.io/docs/html5/#_timing
 
