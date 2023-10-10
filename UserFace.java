f349a4ea7ab5ce37c3ee0b9b6897748d4418b77b
Sofiya_Chubich <sofiachubich@gmail.com>
2023-10-10 12:51:04 -0700

    Panes more like GAINS

--- UserFace.java
+++ UserFace.java
@@ -183,6 +183,7 @@ public class UserFace extends Application implements EventHandler<ActionEvent>{
         grid.add(zeroPane, 1, 7);
 
         textboxPane.getChildren().add(textbox);
+        additionPane.getChildren().add(addition);
 
         GridPane.setColumnSpan(textbox, 5);
 
