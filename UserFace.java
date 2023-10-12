04884896345fce5b8ca4f5bb9c24721cd671cd3c
Sofiya_Chubich <sofiachubich@gmail.com>
2023-10-12 13:06:22 -0700

    eRect

--- UserFace.java
+++ UserFace.java
@@ -313,129 +313,200 @@ public class UserFace extends Application implements EventHandler<ActionEvent>{
 
         TranslateTransition translate_subtraction = new TranslateTransition();  
         translate_subtraction.setNode(subtraction);
+        TranslateTransition translate_subtractionRect = new TranslateTransition();  
+        translate_subtractionRect.setNode(subtractionRect); 
+
         TranslateTransition translate_multiplication = new TranslateTransition();  
         translate_multiplication.setNode(multiplication);
+        TranslateTransition translate_multiplicationRect = new TranslateTransition();  
+        translate_multiplicationRect.setNode(multiplicationRect); 
+
         TranslateTransition translate_division = new TranslateTransition();  
         translate_division.setNode(division);
+        TranslateTransition translate_divisionRect = new TranslateTransition();  
+        translate_divisionRect.setNode(divisionRect); 
+
         TranslateTransition translate_equals = new TranslateTransition();  
         translate_equals.setNode(equals);
+        TranslateTransition translate_equalsRect = new TranslateTransition();  
+        translate_equalsRect.setNode(equalsRect); 
+
         TranslateTransition translate_zero = new TranslateTransition();  
         translate_zero.setNode(zero);
+        TranslateTransition translate_zeroRect = new TranslateTransition();  
+        translate_zeroRect.setNode(zeroRect); 
+
         TranslateTransition translate_one = new TranslateTransition();  
         translate_one.setNode(one);
+        TranslateTransition translate_oneRect = new TranslateTransition();  
+        translate_oneRect.setNode(oneRect);
+
         TranslateTransition translate_two = new TranslateTransition();  
         translate_two.setNode(two);
+        TranslateTransition translate_twoRect = new TranslateTransition();  
+        translate_twoRect.setNode(twoRect); 
+
         TranslateTransition translate_three = new TranslateTransition();  
         translate_three.setNode(three);
+        TranslateTransition translate_threeRect = new TranslateTransition();  
+        translate_threeRect.setNode(threeRect); 
+
         TranslateTransition translate_four = new TranslateTransition();  
         translate_four.setNode(four);
+        TranslateTransition translate_fourRect = new TranslateTransition();  
+        translate_fourRect.setNode(fourRect); 
+
         TranslateTransition translate_five = new TranslateTransition();  
         translate_five.setNode(five);
+        TranslateTransition translate_fiveRect = new TranslateTransition();  
+        translate_fiveRect.setNode(fiveRect); 
+
         TranslateTransition translate_six = new TranslateTransition();  
         translate_six.setNode(six);
+        TranslateTransition translate_sixRect = new TranslateTransition();  
+        translate_sixRect.setNode(sixRect); 
+
         TranslateTransition translate_seven = new TranslateTransition();  
         translate_seven.setNode(seven);
+        TranslateTransition translate_sevenRect = new TranslateTransition();  
+        translate_sevenRect.setNode(sevenRect); 
+
         TranslateTransition translate_eight = new TranslateTransition();  
         translate_eight.setNode(eight);
+        TranslateTransition translate_eightRect = new TranslateTransition();  
+        translate_eightRect.setNode(eightRect); 
+
         TranslateTransition translate_nine = new TranslateTransition();  
         translate_nine.setNode(nine);
+        TranslateTransition translate_nineRect = new TranslateTransition();  
+        translate_nineRect.setNode(nineRect); 
+
         TranslateTransition translate_e = new TranslateTransition();  
         translate_e.setNode(e);
+        TranslateTransition translate_eRect = new TranslateTransition();  
+        translate_eRect.setNode(eRect); 
+
         TranslateTransition translate_pi = new TranslateTransition();  
         translate_pi.setNode(pi);
+        TranslateTransition translate_piRect = new TranslateTransition();  
+        translate_piRect.setNode(piRect); 
+
         TranslateTransition translate_sin = new TranslateTransition();  
         translate_sin.setNode(sin);
+        TranslateTransition translate_sinRect = new TranslateTransition();  
+        translate_sinRect.setNode(sinRect); 
+
         TranslateTransition translate_cos = new TranslateTransition();  
         translate_cos.setNode(cos);
+        TranslateTransition translate_cosRect = new TranslateTransition();  
+        translate_cosRect.setNode(cosRect); 
+        
         TranslateTransition translate_tan = new TranslateTransition();  
         translate_tan.setNode(tan);
+        TranslateTransition translate_tanRect = new TranslateTransition();  
+        translate_tanRect.setNode(tanRect); 
+
         TranslateTransition translate_root = new TranslateTransition();  
         translate_root.setNode(root);
+        TranslateTransition translate_rootRect = new TranslateTransition();  
+        translate_rootRect.setNode(rootRect); 
+
         TranslateTransition translate_log = new TranslateTransition();  
         translate_log.setNode(log);
+        TranslateTransition translate_logRect = new TranslateTransition();  
+        translate_logRect.setNode(logRect); 
+
         TranslateTransition translate_exponent = new TranslateTransition();  
         translate_exponent.setNode(exponent);
+        TranslateTransition translate_exponentRect = new TranslateTransition();  
+        translate_exponentRect.setNode(exponentRect); 
+
         TranslateTransition translate_openParentheses = new TranslateTransition();
         translate_openParentheses.setNode(openParentheses);
+        TranslateTransition translate_openParenthesesRect = new TranslateTransition();  
+        translate_openParenthesesRect.setNode(openParenthesesRect); 
+
         TranslateTransition translate_closeParentheses = new TranslateTransition();
         translate_closeParentheses.setNode(closeParentheses);
+        TranslateTransition translate_closeParenthesesRect = new TranslateTransition();  
+        translate_closeParenthesesRect.setNode(closeParentheses); 
 
 
         additionRect.setOnMouseEntered(event -> {
             move(translate_addition, translate_additionRect, addition, additionRect);
             });
-        //subtraction.setOnMouseEntered(event -> {
-        //    move(translate_subtraction, subtraction, additionRect);
-        //    });
-        //multiplication.setOnMouseEntered(event -> {
-        //    move(translate_multiplication, multiplication, additionRect);
-        //    });
-        //division.setOnMouseEntered(event -> {
-        //    move(translate_division, division, additionRect);
-        //    }); 
-        //one.setOnMouseEntered(event -> {
-        //    move(translate_one, one);
-        //    }); 
-        //two.setOnMouseEntered(event -> {
-        //    move(translate_two, two);
-        //    }); 
-        //three.setOnMouseEntered(event -> {
-        //    move(translate_three, three);
-        //    }); 
-        //four.setOnMouseEntered(event -> {
-        //    move(translate_four, four);
-        //    }); 
-        //five.setOnMouseEntered(event -> {
-        //    move(translate_five, five);
-        //    }); 
-        //six.setOnMouseEntered(event -> {
-        //    move(translate_six, six);
-        //    }); 
-        //seven.setOnMouseEntered(event -> {
-        //    move(translate_seven, seven);
-        //    }); 
-        //eight.setOnMouseEntered(event -> {
-        //    move(translate_eight, eight);
-        //    }); 
-        //nine.setOnMouseEntered(event -> {
-        //    move(translate_nine, nine);
-        //    }); 
-        //zero.setOnMouseEntered(event -> {
-        //    move(translate_zero, zero);
-        //    }); 
-        //equals.setOnMouseEntered(event -> {
-        //    move(translate_equals, equals);
-        //    }); 
-        //sin.setOnMouseEntered(event -> {
-        //    move(translate_sin, sin);
-        //    }); 
-        //cos.setOnMouseEntered(event -> {
-        //    move(translate_cos, cos);
-        //    }); 
-        //tan.setOnMouseEntered(event -> {
-        //    move(translate_tan, tan);
-        //    }); 
-        //e.setOnMouseEntered(event -> {
-        //    move(translate_e, e);
-        //    }); 
-        //exponent.setOnMouseEntered(event -> {
-        //    move(translate_exponent, exponent);
-        //    }); 
-        //pi.setOnMouseEntered(event -> {
-        //    move(translate_pi, pi);
-        //    }); 
-        //log.setOnMouseEntered(event -> {
-        //    move(translate_log, log);
-        //    }); 
-        //root.setOnMouseEntered(event -> {
-        //    move(translate_root, root);
-        //    });
-        //openParentheses.setOnMouseEntered(event -> {
-        //    move(translate_openParentheses, openParentheses);
-        //    });
-        //closeParentheses.setOnMouseEntered(event -> {
-        //    move(translate_closeParentheses, closeParentheses);
-        //    });
+        subtraction.setOnMouseEntered(event -> {
+            move(translate_subtraction, translate_subtractionRect, subtraction, additionRect);
+            });
+        multiplication.setOnMouseEntered(event -> {
+            move(translate_multiplication, translate_multiplicationRect, multiplication, additionRect);
+            });
+        division.setOnMouseEntered(event -> {
+            move(translate_division, translate_divisionRect, division, additionRect);
+            }); 
+        one.setOnMouseEntered(event -> {
+            move(translate_one, translate_one, one, oneRect);
+            }); 
+        two.setOnMouseEntered(event -> {
+            move(translate_two, translate_two, two, twoRect);
+            }); 
+        three.setOnMouseEntered(event -> {
+            move(translate_three, translate_threeRect, three, threeRect);
+            }); 
+        four.setOnMouseEntered(event -> {
+            move(translate_four, translate_fourRect, four, fourRect);
+            }); 
+        five.setOnMouseEntered(event -> {
+            move(translate_five, translate_fiveRect, five, fiveRect);
+            }); 
+        six.setOnMouseEntered(event -> {
+            move(translate_six, translate_sixRect, six, sixRect);
+            }); 
+        seven.setOnMouseEntered(event -> {
+            move(translate_seven,  translate_sevenRect, seven, sevenRect);
+            }); 
+        eight.setOnMouseEntered(event -> {
+            move(translate_eight, translate_eightRect, eight, eightRect);
+            }); 
+        nine.setOnMouseEntered(event -> {
+            move(translate_nine, translate_nineRect, nine, nineRect);
+            }); 
+        zero.setOnMouseEntered(event -> {
+            move(translate_zero, translate_zeroRect, zero, zeroRect);
+            }); 
+        equals.setOnMouseEntered(event -> {
+            move(translate_equals, translate_equalsRect, equals, equalsRect);
+            }); 
+        sin.setOnMouseEntered(event -> {
+            move(translate_sin, translate_sinRect, sin, sinRect);
+            }); 
+        cos.setOnMouseEntered(event -> {
+            move(translate_cos, translate_cosRect, cos, cosRect);
+            }); 
+        tan.setOnMouseEntered(event -> {
+            move(translate_tan, translate_tanRect, tan, tanRect);
+            }); 
+        e.setOnMouseEntered(event -> {
+            move(translate_e, translate_eRect, e, eRect);
+            }); 
+        exponent.setOnMouseEntered(event -> {
+            move(translate_exponent, translate_exponentRect, exponent, exponentRect);
+            }); 
+        pi.setOnMouseEntered(event -> {
+            move(translate_pi, translate_piRect, pi, piRect);
+            }); 
+        log.setOnMouseEntered(event -> {
+            move(translate_log, translate_logRect, log, logRect);
+            }); 
+        root.setOnMouseEntered(event -> {
+            move(translate_root, translate_rootRect, root, rootRect);
+            });
+        openParentheses.setOnMouseEntered(event -> {
+            move(translate_openParentheses, translate_openParenthesesRect, openParentheses, openParenthesesRect);
+            });
+        closeParentheses.setOnMouseEntered(event -> {
+            move(translate_closeParentheses, translate_closeParenthesesRect, closeParentheses, closeParenthesesRect);
+            });
     }
 
     @Override
