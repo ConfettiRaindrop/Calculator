3eeecd4322f9381167672286f8b42a889eafee36
Sindhura-Satti <89949740+Sindhura-Satti@users.noreply.github.com>
2023-10-19 20:19:36 +0000

    line 30, using helper methods for repition

--- Translator.java
+++ Translator.java
@@ -2,17 +2,16 @@ import java.util.ArrayList;
 public class Translator {
     public static String u_input;
     public static String h_u_input;
+    public static char[] equation; 
 
     Translator(){
       //place holder u_input so i test out my functions
         u_input = "3+8*2";
-        h_u_input = "9 + ( 3 * ( 6 + 10 + ( 9 * 0 ) ) )"; //how should i make 10 count as one num and not as 1 and 0?
-        System.out.println("Hello world"); 
-        //CalculatorFunctions calc = new CalculatorFunctions(); 
-        //String u_input = getUserInput(); 
+        h_u_input = "9 + ( 3 * ( 6 + 10 + ( 9 * 0 ) ) )"; //how should i make 10 count as one num and not as 1 and 0? 
+        equation = u_input.toCharArray(); 
     }
   public static void main(String[] args) {
-  	char[] equation = u_input.toCharArray(); 
+  	
   	ArrayList<Character> operators = new ArrayList<>(); 
     ArrayList<Integer> nums = new ArrayList<>();
 
@@ -28,20 +27,8 @@ public class Translator {
 
           }else if(String.valueOf(n).matches("[^a-zA-Z0-9]")){
             // The parameters im passing into the matches method above is from stack overflow:  https://stackoverflow.com/questions/1721602/regex-for-matching-a-z-a-z-0-9-and 
-            if (n == '(') {
-              //loop through till you find the end parenthesis and pass in the seperated eq.
-              for (int j = i; j < equation.length; j++){
-                n = equation[j]; 
-                if (n == ')'){
-                  int ultRizz = Translator.recurSolve(equation,i,j);
-                  nums.add(ultRizz);
-                  i = j; 
-                  break;  
-                }
-              }
-            }
-
             operators.add(n);
+
           }else{
             //not a charecter 
           }
@@ -57,22 +44,50 @@ public class Translator {
         return equation[0]; 
 
       }else{
-        char s = equation[0];
-        if (Character.isDigit(s)){
-          String tempNumString = ""; 
-          rNums.add(Character.getNumericValue(s));
+        for(int e = 0; e < equation.length; e++){
+          char s = equation[e];
 
-        }else if (Character.isWhitespace(s)){
-          // continue; (in loop)
-        }else if (String.valueOf(s).matches("[^a-zA-Z0-9]")){
-          rOperators.add(s);
-        }else{
-          //weird characters 
+          //accounts for a number 
+          if (Character.isDigit(s)){
+            wholeNum(s, e); 
+
+          //accounts for opperators
+          }else if (String.valueOf(s).matches("[^a-zA-Z0-9]")){
+            rOperators.add(s);
+        
+          // other non-operator character - jus so our code don't break
+          }else{
+            System.out.println("weird character input-ed");
+          }
         }
-      }
-      return 0; 
+      
     }
 
+    public static int parenthesesEd(int index){
+      if (n == '(') {
+              //loop through till you find the end parenthesis and pass in the seperated eq.
+              for (int j = i; j < equation.length; j++){
+                n = equation[j]; 
+                if (n == ')'){
+                  int ultRizz = Translator.recurSolve(equation,i,j);
+                  nums.add(ultRizz);
+                  i = j; 
+                  break;  
+                }
+              }
+    } 
+
+    public static int wholeNum(char s, int index){
+      String tempNumString = "" + s; 
+          for(int d = index; d < equation.length; d++){
+            if (Character.isDigit(equation[d])){
+              tempNumString = tempNumString + equation[d]; 
+            }else{
+              break; 
+            }
+          }
+          return Integer.parseInt(tempNumString);
+    }
 
     public String getInput(String u_input){
       return u_input; 
