11fe3e9d78c98d547344d254045cd457d726bfd5
Sindhura-Satti <89949740+Sindhura-Satti@users.noreply.github.com>
2023-10-22 16:35:39 -0700

    Update Translator.java

--- Translator.java
+++ Translator.java
@@ -4,17 +4,17 @@ public class Translator {
     public static String h_u_input;
     public static char[] equation;
     public static ArrayList<Character> operators;
-    public static ArrayList<Integer> nums;
-    private static CalculatorFunctions calc; 
+    public static ArrayList<Double> nums;
+    //private static CalculatorFunctions calc; 
 
     Translator(){
       //place holder u_input so i test out my functions
-        u_input = "3+8*2";
+        u_input = "3+8*2"; //todo replace with getInput() 
         h_u_input = "9 + ( 3 * ( 6 + 10 + ( 9 * 0 ) ) )"; //how should i make 10 count as one num and not as 1 and 0? - wholeNum()
         equation = u_input.toCharArray();
         operators = new ArrayList<>(); 
         nums = new ArrayList<>();
-        calc = new CalculatorFunctions();
+        //calc = new CalculatorFunctions(); 
     }
   public static void main(String[] args) {
 
@@ -22,15 +22,20 @@ public class Translator {
         // changing the for each loop to a regular for bc i need to pass in a whole section of the equation that was in parenthesis and for that I 
           char n = equation[i]; 
           if (Character.isDigit(n)){
-               int n_converted = n; 
-              nums.add(n_converted);
+              double [] numInds = wholeNum(n, i);
+              nums.add(numInds[1]);
+              i = (int)numInds[0];
 
           }else if (Character.isWhitespace(n)){
             continue; 
 
           }else if(String.valueOf(n).matches("[^a-zA-Z0-9]")){
             // The parameters im passing into the matches method above is from stack overflow:  https://stackoverflow.com/questions/1721602/regex-for-matching-a-z-a-z-0-9-and 
-            operators.add(n);
+            if (n == '('){
+              parenthesesEd(i); 
+            }else{
+              operators.add(n);
+            }
 
           }else{
             //not a charecter 
@@ -38,8 +43,9 @@ public class Translator {
           }
           
       }
+      getResult(compiled(nums, operators)); 
     }
-    public static int parenthesesEd(int ind){
+    public static double parenthesesEd(int ind){
       
       //loop through till you find the end parenthesis and pass in the seperated eq.
       //if you find another open parenthesis, recurse through the method again
@@ -50,65 +56,103 @@ public class Translator {
           parenthesesEd(j+1);
         }
         if (n == ')'){
-          int rInd = j+1;
-          return recurSolve(ind, j); // j-1 ?
+          return sortSolve(ind, j); // j-1 ?
            
         }
       }
-      // returning result from recurSolve (0) and end index after parenthesis -rInd (1)
+      // returning result from sortSolve (0) and end index after parenthesis -rInd (1)
     }
-    // what happens when there isn't a 
 
-    public static int recurSolve( int i, int j){
-      ArrayList<Integer> rNums = new ArrayList<>();
+    public static double sortSolve( int i, int j){
+      ArrayList<Double> rNums = new ArrayList<>();
       ArrayList<Character> rOperators = new ArrayList<>();
       //if statement to return a single 
-      if (not(equation.matches("*[^a-zA-Z0-9]*"))){
-        if (Character.isDigit(equation[0])){
-          int ultRizz = (int)equation[0];
-          return ultRizz; 
+      // if (operators.isEmpty()){//fix
+      //   double [] vals = wholeNum(equation[i], i); 
+      //   nums.set()
+      //   if (nums.size() == ){
+      //     double ultRizz = (double)equation[0];
+      //     return ultRizz; 
+      //   }else{
+      //     return 666; 
+      //   }   
+
+      // }else{
+      for(int e = i; e < j; e++){
+        char s = equation[e];
+
+        //accounts for a number 
+        if (Character.isDigit(s)){
+          double[] numRizz = wholeNum(s, e);
+          rNums.add(numRizz[1]); 
+          e = (int)numRizz[0];  //index skips to end of multidigit number
+
+        //accounts for opperators
+        }else if (String.valueOf(s).matches("[^a-zA-Z0-9]")){
+          //this method will be passed after parentheses are accounted for 
+          rOperators.add(s);
+          
         }else{
-          return 666; 
-        }   
+          // other non-operator character - jus so our code don't break
+          return 666;
 
-      }else{
-        for(int e = i; e < j; e++){
-          char s = equation[e];
-
-          //accounts for a number 
-          if (Character.isDigit(s)){
-            int[] numRizz = wholeNum(s, e);
-            rNums.add(numRizz[1]); 
-            e = numRizz[0];  //index skips to end of multidigit number
-
-          //accounts for opperators
-          }else if (String.valueOf(s).matches("[^a-zA-Z0-9]")){
-            //this method will be passed after parentheses are accounted for 
-            rOperators.add(s);
-            
-          }else{
-            // other non-operator character - jus so our code don't break
-            return 666; 
-
-          }
         }
-        //evaluate
-        return compiled(rNums,rOperators);
-
       }
+        //evaluate
+      return compiled(rNums,rOperators);
+
+      //}
       
     }
-    public static int compiled(ArrayList<Integer> nums, ArrayList<Character> ops){
+    public static double compiled(ArrayList<Double> nums, ArrayList<Character> ops){
       if (nums.size() <= ops.size()){
-        return 666;
+        return 666.6;
+      }else if (nums.size() == 1){
+        return nums.get(0);
       }else{
         for(int i = 0; i < ops.size(); i++){
+          if (ops.get(i) == '^'){
+            nums.set(i, (double)CalculatorFunctions.power(nums.get(i), nums.get(i + 1)));
+            nums.remove(i + 1);
 
-          if (ops[i] == '^'){
-            calc.power(nums.remove(i), nums.remove(i+1)); 
+            ops.remove(i);
+          }
+        }
+        for(int i = 0; i < ops.size(); i++){
+          char op = ops.get(i); 
+          if (op == '*' || op == '/'){
+            if (op == '*'){
+              nums.set(i, (double)CalculatorFunctions.multiply(nums.get(i), nums.get(i + 1)));
+              
+            }else if (op == '/'){
+              nums.set(i, (double)CalculatorFunctions.divide(nums.get(i), nums.get(i + 1)));
+              
+            }
+            nums.remove(i + 1);
+            ops.remove(i);
+
           }
         }
 
+        for(int i = 0; i < ops.size(); i++){
+          char op = ops.get(i); 
+          if (op == '+' || op == '-'){
+            if (op == '+'){
+              nums.set(i, (double)CalculatorFunctions.summ(nums.get(i), nums.get(i + 1)));
+              
+            }else if (op == '-'){
+              nums.set(i, (double)CalculatorFunctions.subtract(nums.get(i), nums.get(i + 1)));
+              
+            }
+            nums.remove(i + 1);
+            ops.remove(i);
+
+          }
+        }
+        if (ops.size() > 0){
+          return compiled(nums, ops); 
+        }
+        return 666.6; 
       }
     }
 
@@ -116,31 +160,34 @@ public class Translator {
       if (nums.size() <= operators.size()){
         return "invalid input";
       }else{
-        return Integer.toString(compiled(nums,operators)); 
+        return Double.toString(compiled(nums,operators)); 
       }
     }
      
 
-    public static int[] wholeNum(char s, int index){
-      // returns an array: ind 0 = full number, ind 1 = index of equation after number 
+    public static double[] wholeNum(char s, int index){
+      // returns an array: ind 1 = full number, ind 0 = index of equation after number 
       String tempNumString = "" + s; 
-      int [] rAr = new int[2];
+      double [] rAr = new double[2];
       for(int d = index+1; d < equation.length; d++){
-        if (Character.isDigit(equation[d])){
+        if (Character.isDigit(equation[d]) || (equation[d] == '.')){
           tempNumString = tempNumString + equation[d]; 
         }else{
           rAr[0] = d-1;  
           break; 
         }
       }
-      rAr[1] = Integer.parseInt(tempNumString);
+      rAr[1] = Double.parseDouble(tempNumString);
       return rAr; 
     }
 
     public String getInput(String u_input){
       return u_input; 
     }
-  
+    
+    public static String getResult(double rizz){
+      return Double.toString(rizz); 
+    }
   
 
 
